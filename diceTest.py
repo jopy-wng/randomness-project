c8d5c7991cefe9cadb8368622c2b94c86ac8f5e7
Jophy Wang <jophyw2008@gmail.com>
2025-11-05 15:17:52 -0500

    bar graphs added

--- diceTest.py
+++ diceTest.py
@@ -1,7 +1,14 @@
 Web VPython 3.2
 import random
 
-numOfDice = int(input("How many dice would you like? (1–10): "))
+inp = int(input("How many dice would you like? (1–10): "))
+
+numOfDice = 0
+
+if inp > 10:
+    numOfDice = 10
+else:
+    numOfDice = inp
 
 numOfDice = max(1, min(numOfDice, 10))
 
@@ -20,7 +27,7 @@ spinTime = 0
 while spinTime < 1.5:
     rate(60)
     spinTime += 0.02
-    # enumerate() function in Python is a built-in function that adds a counter to an iterable (like a list, tuple, or string) and returns an enumerate object.
+    # enumerate() in Python is a built-in function that adds a counter to an iterable (like a list, tuple, or string) and returns an enumerate object.
     for i, d in enumerate(dice_list):
         d.rotate(angle = 0.3, axis = vector(random.random(), random.random(), random.random()))
 
@@ -33,6 +40,22 @@ def randOr(dice):
 for d in dice_list:
     randOr(d)
 
-results = [random.randint(1, 6) for i in range(numOfDice)]
+results = [random.randint(1, 6) for i in range(inp)]
 print(results)
 
+counterOne = 1
+counterTwo = 1
+counterThree = 1
+counterFour = 1
+counterFive = 1
+counterSix = 1
+
+gbar = graph(title = "Dice Roll Frequencies", xtitle = "Possible Sides", ytitle = "Times Rolled")
+bar_graph = gvbars(delta = 1, color = color.red)
+for i in range(len(results)):
+    if results[i] == 1: bar_graph.plot(pos = (1, counterOne)) counterOne += 1
+    if results[i] == 2: bar_graph.plot(pos = (2, counterTwo)) counterTwo += 1
+    if results[i] == 3: bar_graph.plot(pos = (3, counterThree)) counterThree += 1
+    if results[i] == 4: bar_graph.plot(pos = (4, counterFour)) counterFour += 1
+    if results[i] == 5: bar_graph.plot(pos = (5, counterFive)) counterFive += 1
+    if results[i] == 6: bar_graph.plot(pos = (6, counterSix)) counterSix += 1
